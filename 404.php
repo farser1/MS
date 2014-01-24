824bd743e0a9570e79e07c710ffbccd4c0a5ee78
John Sawers <johnrsawers@gmail.com>
2014-01-24 22:51:10 +0000

    Added 404 pages

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php 
+$pageTitle="404 Page not found";
+include('inc/header.php'); 
+?>
+
+  <div class="scn_benefits">
+    <div class="w-container ctr_benefits">
+      <h1 class="hdr_benefits hdr_benefitspage">Sorry, we can't find that page. (404)</h1>
+    </div>
+    <div class="w-container ctn_privacy">
+      <p class="privacy">
+Would these links help you?
+<br>
+<br>
+<a href="http://joinmyteam.co">Home</a>
+<br>
+<a href="http://joinmyteam.co/courses.php">View all courses</a>
+<br>
+<a href="http://joinmyteam.co/blog">Blog</a>
+<br>
+<br>
+If you followed a broken link, please <a href="mailto:hello@joinmyteam.co">let us know</a> where the broken link came from.
+      <p>
+    </div>
+  </div>
+
+<?php include('inc/footer.php'); ?>
