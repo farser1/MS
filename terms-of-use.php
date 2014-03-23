6917bf4f97f2350c81317a6d4b284d7e15288e67
farser <johnrsawers@gmail.com>
2014-03-23 14:33:03 +0000

    Terms of Use added

---
+++ terms-of-use.php
@@ -0,0 +1,102 @@
+<?php 
+$pageTitle="Terms of Use";
+$pageDesc="";
+include('inc/header.php'); ?>
+
+  <div class="section section-title" id="top">
+    <div class="w-container">
+      <h1 class="page-title">Terms of Use</h1>
+    </div>
+  </div>
+  <div class="section section-white">
+    <div class="w-container">
+      <p>Welcome to joinmteam.co "Our Site". To look after your privacy and to keep the content useful, visitors and account holders must follow the rules in this Terms of Use Agreement and our Privacy Statement. By using Our Site, either as a visitor or an account holder, you are deemed to have accepted these Terms of Use and our Privacy Statement.</p>
+	  <h3>Disclaimer</h3>
+<p>You should always consult your doctor before starting any new exercise program. This is particularly true if any of the following apply to you:
+<br><br>
+You experience chest pain<br>
+You feel faint of have spells of dizziness<br>
+You have joint of bone problems<br>
+You have high blood pressure<br>
+You have a heart condition<br>
+You are pregnant<br>
+You are currently taking medication<br>
+You are not used to being very physically active</p> 
+
+
+<p>If none of these apply to you can be reasonably sure that you can become more physically active. If you begin to experience any of the physical symptoms listed above or your health situation changes you should contact your doctor immediately.</p>
+
+<p>If any of the above conditions apply to you then you should consult your doctor before becoming more physically active. Tell your doctor which of the conditions applied to you and explain a little about the kind of activity you want to participate in. You may be able to do any activity you want-as long as you begin slowly and build up gradually, or you may need to restrict your activities to those which are safe for you.</p>
+
+<p>The information and materials contained on the platform are for educational and informational purposes only. Content on joinmyteam.co and our Trainers does not replace the relationship between you and doctors or other healthcare professionals nor the advice you receive from them.</p>
+
+<p>By agreeing to our terms of use you acknowledge that you have either had a physical examination and been given your doctor’s permission to participate or, if you you have chosen not to obtain your doctor's permission you acknowledge that you are doing so at your own risk. You agree to waive any and all claims against MyTeam and our trainers except from exclusions defined in the "Our Liability" section.</p>
+                
+<h3>Your Privacy</h3>
+<p>We process information about you in accordance with our privacy policy (http://joinmyteam.co/privacy-policy).  By using our site, you consent to such processing and you warrant that all data provided by you is accurate.</p>
+
+<h3>Fees and Subscriptions</h3>
+<p>We reserve the right to charge a fee for accessing our site and content. Payment for these fee will be taken through third party payment handlers and is subject to their terms and conditions. We will never store you payment details, however they may be stored by our partners. You may cancel your subscription at any time by email hello@joinmyteam.co. We reserve the right to remove your account and cancel your subscription at our discretion. In this instance no refund will be provided for previous subscription payments and no further payments will be taken. Any future fees already paid will be refunded.<p>
+
+<h3>Acceptable use and User Generated Content</h3>
+<p>Your contributions must:<br>
+be accurate (where they state facts);<br>
+be genuinely held (where they state opinions); and<br>
+comply with applicable law in the UK and in any country from which they are posted.<br>
+<br>
+Contributions must not:<br>
+contain any material that is obscene, offensive, hateful, inflammatory or defamatory of any person;<br>
+promote any discrimination based on race, sex, religion, nationality, disability, sexual orientation or age;<br>
+infringe any copyright, database right, trade mark or any other right of any other person;<br>
+be threatening, abuse or invade another's privacy, or cause annoyance, inconvenience or needless anxiety;<br>
+be likely to harass, upset, embarrass alarm or annoy any other person;<br>
+be used to impersonate any person, or to misrepresent your identity or affiliation with any person;<br>
+give the impression that they emanate from us, if this is not the case; or<br>
+advocate, promote or assist any unlawful act such as (by way of example only) copyright infringement or computer misuse.</P>
+
+<p>We do not actively moderate this site, when issues are brought to our attention we may determine, at our discretion, whether there has been a breach of these Terms of Use. We reserve the right to remove your account and any content submitted by you at our discretion.</p>
+
+<h3>Our liability</h3>
+<p>While we make every effort to keep joinmyteam.co up to date, we don’t provide any guarantees, conditions or warranties as to the accuracy of the information on the site.</p>
+
+<p>While we make every effort to check the qualifications and backgrounds of the Trainers on our platform you are solely responsible for determining the identity and suitability of other people whom you may contact by means of the Platform.</p>
+
+<p>We don’t accept liability for loss or damage incurred by users of the website, whether direct, indirect or consequential, whether caused by tort, breach of contract or otherwise, in connection with our site, its use, the inability to use, or results of the use of our site, any websites linked to it and any materials posted on it.</p>
+
+<p>This includes loss of:<br>
+income or revenue<br>
+business<br>
+profits or contracts<br>
+anticipated savings<br>
+data<br>
+goodwill<br>
+tangible property<br>
+wasted management or office time</P>
+
+<p>This does not affect our liability for death or personal injury arising from our negligence, nor our liability for fraudulent misrepresentation or misrepresentation as to a fundamental matter, nor any other liability which cannot be excluded or limited under applicable law.</p>
+
+<h3>Waiver</h3>
+<p>If we fail, at any time during the term of your use of joinmyteam.co to insist upon strict performance of any of your obligations under these terms of use, or if we fail to exercise any of the rights to which we are entitled under these terms of use, this shall not constitute a waiver of such rights or remedies and shall not relieve you from compliance with such obligations.</p>
+
+<h3>Security</h3>
+<p>You agree to notify hello@joinmyteam.co immediately of any unauthorized use of your account or any other breach of security. We will not be liable for any loss or damage that you may incur as a result of someone else using your password or account. However, you could be held liable for loss or damage incurred by us or another party due to someone else using your account or password. You may not use anyone else's account at any time, without the express permission of the account holder. If you think that someone else has access to your account you should contact us immediately and change your password.</p>
+
+<h3>Revisions to these terms</h3>
+<p>We may at any time revise these terms of use without notice. Please check these terms of use regularly, as continued use of the joinmyteam.co website after a change has been made is your acceptance of the change.</p>
+
+<h3>Severability</h3>
+<p>If any of these terms and conditions or any provisions of these terms and conditions are determined by any competent authority to be invalid, unlawful or unenforceable to any extent, such term, condition or provision will to that extent be severed from the remaining terms, conditions and provisions, which will continue to be valid to the fullest extent permitted by law.<p>
+
+<h3>Law and jurisdiction</h3>
+<p>These terms of use and the agreement between you and us will be governed by English law. Any dispute arising from, or related to, these terms of use shall be subject to the non-exclusive jurisdiction of the courts of England and Wales.</p>
+
+<h3>Your concerns</h3>
+<p>If you have any concerns about material which appears on our site or these terms, please contact hello@joinmyteam.co.</p>
+<p>Thank you for visiting our site.</p>
+
+    </div>
+  </div>
+ 
+
+<?php include('inc/footer.php'); ?>
+
